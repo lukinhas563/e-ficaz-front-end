5d7219c1d3b096960c4c3ba377a05e49cce72c77
Lucas Montenegro <lucasmontenegro475@gmail.com>
2024-04-15 10:31:30 -0300

    Created company page tables

---
+++ src/lib/components/TableRegister.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	const registers = [
+		{ id: 1, type: 'Fechamento fiscal', date: '05/01/2023', valor: 5123.55 },
+		{ id: 1, type: 'Fechamento fiscal', date: '05/01/2023', valor: 5123.55 },
+		{ id: 1, type: 'Fechamento fiscal', date: '05/01/2023', valor: 5123.55 },
+		{ id: 1, type: 'Fechamento fiscal', date: '05/01/2023', valor: 5123.55 },
+		{ id: 1, type: 'Fechamento fiscal', date: '05/01/2023', valor: 5123.55 },
+		{ id: 1, type: 'Fechamento fiscal', date: '05/01/2023', valor: 5123.55 },
+		{ id: 1, type: 'Fechamento fiscal', date: '05/01/2023', valor: 5123.55 },
+		{ id: 1, type: 'Fechamento fiscal', date: '05/01/2023', valor: 5123.55 },
+		{ id: 1, type: 'Fechamento fiscal', date: '05/01/2023', valor: 5123.55 }
+	];
+</script>
+
+<table class="main-table">
+	<thead class="table-header">
+		<tr>
+			<th scope="col">ID</th>
+			<th scope="col">Fechamento</th>
+			<th scope="col">Data</th>
+			<th scope="col">Valor</th>
+		</tr>
+	</thead>
+	<tbody class="table-body">
+		{#each registers as register}
+			<tr>
+				<td class="id">{register.id}</td>
+				<td>{register.type}</td>
+				<td>{register.date}</td>
+				<td>R$ {register.valor}</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
+
+<style>
+	.main-table {
+		width: 100%;
+
+		border-collapse: collapse;
+	}
+
+	/* HEADER */
+	.table-header tr {
+		border-bottom: 1px solid var(--table-line);
+	}
+	.table-header tr th {
+		height: 50px;
+
+		color: var(--table-line);
+
+		font-weight: normal;
+	}
+
+	/* HEADER */
+	.table-body tr {
+		height: 40px;
+
+		border-bottom: 1px solid var(--table-line);
+
+		cursor: pointer;
+
+		text-align: center;
+	}
+	.table-body tr:hover {
+		background-color: var(--table-hover);
+	}
+</style>
--- src/routes/[userId]/[collaboratorId]/companies/[companyId]/+page.svelte
+++ src/routes/[userId]/[collaboratorId]/companies/[companyId]/+page.svelte
@@ -1,1 +1,157 @@
-<h1>Empresa</h1>
+<script lang="ts">
+	import TableEmployee from '$lib/components/TableEmployee.svelte';
+	import TableRegister from '$lib/components/TableRegister.svelte';
+</script>
+
+<div class="container-info">
+	<div class="container-info-company">
+		<h1>Empresa</h1>
+		<div class="info">
+			<span>
+				<p class="title">Nome empresarial</p>
+				<p>Lucas</p>
+			</span>
+			<span>
+				<p class="title">Nome fantasia</p>
+				<p>Lucas</p>
+			</span>
+			<span>
+				<p class="title">Abertura</p>
+				<p>Lucas</p>
+			</span>
+			<span>
+				<p class="title">CNPJ</p>
+				<p>Lucas</p>
+			</span>
+			<span>
+				<p class="title">Regime</p>
+				<p>Lucas</p>
+			</span>
+			<span>
+				<p class="title">Status</p>
+				<p>Lucas</p>
+			</span>
+		</div>
+	</div>
+	<div>
+		<h2>Endereço</h2>
+		<div class="info">
+			<span>
+				<p class="title">Rua</p>
+				<p>Lucas</p>
+			</span>
+			<span>
+				<p class="title">N°</p>
+				<p>Lucas</p>
+			</span>
+			<span>
+				<p class="title">Complemento</p>
+				<p>Lucas</p>
+			</span>
+			<span>
+				<p class="title">CEP</p>
+				<p>Lucas</p>
+			</span>
+			<span>
+				<p class="title">Bairro</p>
+				<p>Lucas</p>
+			</span>
+			<span>
+				<p class="title">Municipio</p>
+				<p>Lucas</p>
+			</span>
+			<span>
+				<p class="title">UF</p>
+				<p>Lucas</p>
+			</span>
+			<span>
+				<p class="title">E-mail</p>
+				<p>Lucas</p>
+			</span>
+		</div>
+	</div>
+</div>
+<div class="container-table">
+	<div class="container-employee-partners">
+		<div class="container-activity">
+			<span>
+				<p class="title">Atividade economica principal</p>
+				<p>OI</p>
+			</span>
+			<span>
+				<p class="title">Atividade economica secundaria</p>
+				<ul class="activity-list">
+					<li>oi</li>
+					<li>oi</li>
+					<li>oi</li>
+					<li>oi</li>
+				</ul>
+			</span>
+		</div>
+		<div class="container-table-employees-partners">
+			<div class="container-options">
+				<p>Funcionarios</p>
+				<p>Socios</p>
+			</div>
+
+			<div class="table-employee">
+				<TableEmployee />
+			</div>
+		</div>
+	</div>
+	<div class="container-registers">
+		<TableRegister />
+	</div>
+</div>
+
+<style>
+	.activity-list {
+		list-style: none;
+	}
+
+	/* INFO CONTAINER */
+	.container-info {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+	}
+
+	.info {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(5rem, 10rem));
+	}
+
+	.title {
+		color: var(--table-line);
+	}
+
+	/* TABLE CONTAINERS */
+	.container-table {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+	}
+
+	/* container employees and partners */
+	.container-options {
+		display: flex;
+		flex-direction: row;
+
+		gap: 20px;
+
+		padding: 10px 20px;
+	}
+
+	.container-options p {
+		cursor: pointer;
+	}
+	.container-options p:hover {
+		text-decoration: underline;
+	}
+
+	.table-employee {
+		max-width: 100%;
+
+		padding: 10px;
+
+		overflow-x: auto;
+	}
+</style>
